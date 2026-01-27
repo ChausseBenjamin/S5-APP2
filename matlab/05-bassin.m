da5a78a41efe47d0ca219979ff661c6d8537bb06
LyamBRS <cous5830@gmail.com>
2026-01-27 12:08:38 -0500

    Grosse magouille sa mère.

--- matlab/05-bassin.m
+++ matlab/05-bassin.m
@@ -29,6 +29,7 @@ equilibriumXLine = -speedEquilibrium * ones(1, (simulationDuration/timeIncrement
 safeEquilibriumXLine = -safeEquilibriumSpeed * ones(1, (simulationDuration/timeIncrements) +1);
 
 simulatedSafeDepth = 0;
+timeIndexWaterHit = 0;
 
 % - Simulation.
 for i = 1:sizeOfTime-1
@@ -62,6 +63,7 @@ end
 
  % - M¨¦thode de l'APP
 speedEquilibrium = -speedEquilibrium;
+safeEquilibriumSpeed = -safeEquilibriumSpeed;
 speedWhenWaterHit = -sqrt(2*gravity*poolFallHeight);
 taylored = (speedEquilibrium^2) + 2*speedEquilibrium*(speedWhenWaterHit - speedEquilibrium);
 linearA = gravity*(buoyancyConstant-1) + (hydroCoefficient / participantMass) * taylored;
@@ -73,7 +75,7 @@ graphTaylored = (speedEquilibrium.^2) + 2 .* speedEquilibrium.*(appSpeed - speed
 graphLinearA = gravity.*(buoyancyConstant-1) + (hydroCoefficient / participantMass) .* (graphTaylored);
 
 % - Magouille de signe pour le faire afficher a la bonne place
-graphDepth = -((((speedEquilibrium^2) - (appSpeed.^2))/2)./(graphLinearA));
+graphDepth = -((((safeEquilibriumSpeed^2) - (appSpeed.^2))/2)./(graphLinearA));
 graphDepth = graphDepth - graphDepth(end);
 
 
@@ -84,15 +86,18 @@ graphDepth = graphDepth - graphDepth(end);
 %% Graphique de la vitesse en fonction du temps, lin¨¦aris¨¦.
 figure;
 hold on;
-plot(appSpeed, graphDepth, "LineWidth", 2);
-ylim([-10 1]);
-line([speedWhenWaterHit speedWhenWaterHit], ylim, 'color', 'r', 'linewidth', 2, 'linestyle', '--');
-line([speedEquilibrium speedEquilibrium], ylim, 'color', 'b', 'linewidth', 2, 'linestyle', '--');
-line([-safeEquilibriumSpeed -safeEquilibriumSpeed], ylim, 'color', 'g', 'linewidth', 2, 'linestyle', '--');
+plot(appSpeed, graphDepth, "LineWidth", 2, "Color", palette{1});
+ylim([-9 0]);
+xlim([-15 -0.5]);
+line([speedWhenWaterHit speedWhenWaterHit], ylim, 'DisplayName', 'Vitesse frappe eau', 'color', palette{2}, 'linewidth', 2, 'linestyle', '--');
+line([speedEquilibrium speedEquilibrium], ylim, 'DisplayName', 've', 'color', palette{3}, 'linewidth', 2, 'linestyle', '--');
+line([safeEquilibriumSpeed safeEquilibriumSpeed], ylim, 'DisplayName', 've securitaire', 'color', palette{4}, 'linewidth', 2, 'linestyle', '--');
+line(xlim, [depth depth], 'DisplayName', 'profondeur', 'color', palette{5}, 'linewidth', 2, 'linestyle', '--');
 ylabel("Profondeur (m)");
 xlabel("Vitesse (m/s)");
 title("Vitesse en fonction de la profondeur APP");
 grid on;
+legend('show', 'Location', 'best');
 
 %% Graphique vitesse en fonction du temps
 ##figure;
@@ -105,13 +110,20 @@ grid on;
 ##title("Vitesse en fonction du temps");
 ##grid on;
 
+graphDepthShifted = NaN(size(graphDepth));
+shift = timeIndexWaterHit+1;
+graphDepthShifted(shift+1:end) = graphDepth(1:end-shift);
+
 %% Graphique vitesse en fonction de la position
-##figure;
-##plot(time, position, "LineWidth", 2);
-##xlabel("Temps (s)");
-##ylabel("Position (m)");
-##title("Position en fonction du temps");
-##grid on;
+figure;
+hold on;
+plot(time, position, "LineWidth", 2);
+plot(time, -graphDepthShifted - 9, "LineWidth", 2);
+xlabel("Temps (s)");
+ylabel("Position (m)");
+line(xlim, [depth depth], 'DisplayName', 'profondeur', 'color', palette{5}, 'linewidth', 2, 'linestyle', '--');
+title("Position en fonction du temps");
+grid on;
 
 %% Graphique vitesse en fonction de la position
 ##figure;
